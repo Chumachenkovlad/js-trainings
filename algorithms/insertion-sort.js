6e98811e17c109c57f0172def89a85a96aa128c2
Chumachenko Vladyslav <chumachenko.vla@gmail.com>
2019-12-14 17:50:41 +0200

    fix first element issue

--- algorithms/insertion-sort.js
+++ algorithms/insertion-sort.js
@@ -1,10 +1,10 @@
 function insertionSort(numbers) {
-  for (let j = 2; j < numbers.length; j++) {
+  for (let j = 1; j < numbers.length; j++) {
     let key = numbers[j];
 
     let i = j - 1;
 
-    while (i > 0 && numbers[i] > key) {
+    while (i >= 0 && numbers[i] > key) {
       numbers[i + 1] = numbers[i];
       i = i - 1;
     }
@@ -15,4 +15,4 @@ function insertionSort(numbers) {
   return numbers;
 }
 
-console.log(intersectionSort([1, 3, 2, 6, 3, 8, 7]));
+console.log(insertionSort([1, 3, -1, 0, 2, -5, 6, 3, 8, 7]));
